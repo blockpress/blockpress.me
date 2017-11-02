e981cad85767fd34a9c854bc42bbb775726c4fc4
Anthony Channing <antonchanning@gmail.com>
2017-11-02 00:14:46 +0000

    Debugging hjkn modules not loading...

--- hkjn/init.js
+++ hkjn/init.js
@@ -4,10 +4,10 @@ function configFail() {
   console.log('FAILED TO LOAD CONFIG!');
 }
 function parseConfig(config_json) {
+  console.log(config_json);
   config =  config_json;
-
   console.log(config);
-  parseModules(config_json.modules);
+  parseModules(config.modules);
 }
 function getConfigJSON() {
   return $.ajax({

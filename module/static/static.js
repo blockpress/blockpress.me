a2cccc32f13d741ad0b377f0d11b547c6f183ae3
antonchanning <antonchanning@gmail.com>
2018-05-06 16:05:29 +0100

    Static content loads from content folder.
    Issue #17. All static content loads from the content folder now. Makes
    permlinks shorter, and keeps content easier to maintain.

--- module/static/static.js
+++ module/static/static.js
@@ -10,9 +10,10 @@ function static_load(file) {
   pushStateWithoutDuplicate(file, './?p=static/'+file);
   $.ajax({
       type: 'GET',
-      url: file
+      url: "content/"+file
   }).done(static_display);
 }
 function static_permlink(permlink) {
+  permlink = permlink.join("/");
   static_load(permlink);
 }
